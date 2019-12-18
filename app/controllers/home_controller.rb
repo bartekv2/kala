7c076c165a7bddb7599fc16669540e738b64c3c9
Bartosz Dymet <b.dymet@gmail.com>
2019-12-18 20:39:49 +0100

    Add subpages. Add basic css styling

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,19 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def body
+  end
+
+  def coaching
+  end
+
+  def workshops
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # mount Spina::Engine => '/'
+  root 'home#index'
+  get 'o-mnie' => 'home#about', as: :about
+  get 'praca-z-cialem' => 'home#body', as: :body
+  get 'poradnictwo-psychologiczne' => 'home#coaching', as: :coaching
+  get 'warsztaty' => 'home#workshops', as: :workshops
+  get 'kontakt' => 'home#contact', as: :contact
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
