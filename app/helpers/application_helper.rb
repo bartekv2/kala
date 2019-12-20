2606aae4da2303f93e76ec78a1f21c75d2996701
Bartosz Dymet <b.dymet@gmail.com>
2019-12-20 19:29:43 +0100

    Move navbar to partial. Add helpers for footer and language selection. Add favicon.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def show_language_to_select
+    if session[:locale] == "pl"
+      link_to "EN", set_locale_path(locale: "en")
+    else
+      link_to "PL", set_locale_path(locale: "pl")
+    end
+  end
+
+  def show_footer
+    "© #{DateTime.now.year} #{t "copyrights"}"
+  end
+
 end
