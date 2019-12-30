c5f08a018ade55be1b57bfad0d26960674464b1a
Bartosz Dymet <b.dymet@gmail.com>
2019-12-30 17:54:07 +0100

    Merge conflicts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,7 @@
 module ApplicationHelper
 
   def show_language_to_select
-<<<<<<< HEAD
-    if session[:locale] == "pl"
-=======
     if session[:locale] == "pl" || session[:locale].nil? || session[:locale].empty?
->>>>>>> 28fdcbf8860d0ee6ef6b43355a60cc66261a5757
       link_to "EN", set_locale_path(locale: "en")
     else
       link_to "PL", set_locale_path(locale: "pl")
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   theme: {
-<<<<<<< HEAD
-=======
     fontFamily: {
       display: ['Alata', 'sans-serif'],
     }
->>>>>>> 28fdcbf8860d0ee6ef6b43355a60cc66261a5757
     extend: {
       colors: {
         'jozef': '#243c5a',
