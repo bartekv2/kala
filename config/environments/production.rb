93e7c73903b631b983fb5f0290329ab299a9fbde
Bartosz Dymet <b.dymet@gmail.com>
2020-02-05 19:55:15 +0100

    testing mail sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,8 +118,8 @@ Rails.application.configure do
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            ENV["GMAIL_EMAIL"],
-  password:             ENV["GMAIL_PASSWORD"],
+  user_name:            'b.dymet@gmail.com',
+  password:             'l0k0m0tywa',
   authentication:       'plain',
   enable_starttls_auto: true }
   config.action_mailer.default_url_options = { :host => "kala.org.pl" }
